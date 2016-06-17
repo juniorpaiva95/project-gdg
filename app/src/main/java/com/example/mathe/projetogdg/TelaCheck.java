e2c0079ce67692a30894c7007a5db5bdeb7b338f
Junior Paiva <juniorpaiva95@gmail.com>
2016-06-17 00:59:21 -0300

    Projeto GDG concluido

---
+++ app/src/main/java/com/example/mathe/projetogdg/TelaCheck.java
@@ -0,0 +1,13 @@
+package com.example.mathe.projetogdg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_check);
+    }
+}
